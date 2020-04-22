1f8659d0a58c8eaff0f94138e68f67f76e109972
Lyuboslav Asenov <lasenov@vmware.com>
2020-04-22 16:45:08 +0300

    Add equals by reference

---
+++ src/Alat.Utils/ObjectExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Reflection;
+
+namespace Alat.Utils {
+   public static class ObjectExtensions {
+      private static readonly System.Type _ienumerableType = typeof(IEnumerable);
+      public static int GetHashCode(this object self) {
+         var hash = 0;
+         if (self != null) {
+            foreach (var property in self.GetType().GetProperties()) {
+               hash ^= property.GetValue(self)?.GetHashCode() ?? 0;
+            }
+         }
+         return hash;
+      }
+
+      public static bool ReflectionEquals(this object self, object obj) {
+         if (self is null || obj is null) {
+            return self == obj;
+         } else if (self.GetType().Equals(obj.GetType())) {
+
+            foreach (var property in self.
+               GetType().
+               GetProperties(BindingFlags.Public
+                  | BindingFlags.NonPublic
+                  | BindingFlags.Instance
+                  | BindingFlags.DeclaredOnly).
+               Where(p => p.CanRead && (!p.GetIndexParameters()?.Any() ?? true))) {
+               var selfValue = property.GetValue(self);
+               var objValue = property.GetValue(obj);
+               var areEqual = true;
+
+               if (selfValue is null || objValue is null) {
+                  return selfValue == objValue;
+               } else if (_ienumerableType.IsAssignableFrom(selfValue.GetType()) && !(selfValue is string)) {
+                  var selfEnumerator = (selfValue as IEnumerable).GetEnumerator();
+                  var objEnumerator = (objValue as IEnumerable).GetEnumerator();
+
+                  while (true) {
+                     bool selfHasNext = selfEnumerator.MoveNext(),
+                     objHasNext = objEnumerator.MoveNext();
+
+                     if (selfHasNext != objHasNext) {
+                        areEqual = false;
+                        break;
+                     } else if (!selfHasNext) {
+                        break;
+                     } else if (!Equals(selfEnumerator.Current, objEnumerator.Current)) {
+                        areEqual = false;
+                        break;
+                     }
+                  }
+               } else {
+                  areEqual = Equals(selfValue, objValue);
+               }
+
+               if (!areEqual) {
+                  return false;
+               }
+            }
+
+            return true;
+         } else {
+            return false;
+         }
+      }
+   }
+}
---
+++ tests/Alat.Utils.Tests/ObjectExtensionsTests.cs
@@ -0,0 +1,175 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Alat.Utils.Tests {
+   [TestFixture]
+   public class ObjectExtensionsTests {
+      public class DummyClass {
+         public int Prop1 { get; set; }
+         private string Prop2 { get; set; }
+         private string[] Prop3 { get; set; }
+         public List<int> Prop4 { get; set; }
+
+         public DummyClass(int prop1, string prop2, string[] prop3, List<int> prop4) {
+            Prop1 = prop1;
+            Prop2 = prop2;
+            Prop3 = prop3;
+            Prop4 = prop4;
+         }
+
+         public override bool Equals(object obj) {
+            return this.ReflectionEquals(obj);
+         }
+      }
+
+      [Test]
+      public void Equals_ComplexObjects_ReturnsTrue() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+         var obj2 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, obj2);
+
+
+         // ASSERT
+         Assert.IsTrue(result);
+      }
+
+      [Test]
+      public void Equals_SameComplexObject_ReturnsTrue() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, obj1);
+
+
+         // ASSERT
+         Assert.IsTrue(result);
+      }
+
+      [Test]
+      public void Equals_ComplexObjectAndNull_ReturnsFalse() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, null);
+
+
+         // ASSERT
+         Assert.IsFalse(result);
+      }
+
+      [Test]
+      public void Equals_NullAndComplexObject_ReturnsFalse() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(null, obj1);
+
+
+         // ASSERT
+         Assert.IsFalse(result);
+      }
+
+      [Test]
+      public void Equals_DiffComplexObj1_ReturnsFalse() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+         var obj2 = new DummyClass(2, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, obj2);
+
+
+         // ASSERT
+         Assert.IsFalse(result);
+      }
+
+      [Test]
+      public void Equals_DiffComplexObj2_ReturnsFalse() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+         var obj2 = new DummyClass(1, "str1", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, obj2);
+
+
+         // ASSERT
+         Assert.IsFalse(result);
+      }
+
+      [Test]
+      public void Equals_DiffComplexObj3_ReturnsFalse() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+         var obj2 = new DummyClass(1, "str", new[] { "str", "str2" }, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, obj2);
+
+
+         // ASSERT
+         Assert.IsFalse(result);
+      }
+
+      [Test]
+      public void Equals_DiffComplexObj4_ReturnsFalse() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+         var obj2 = new DummyClass(1, "str", new[] { "str1" }, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, obj2);
+
+
+         // ASSERT
+         Assert.IsFalse(result);
+      }
+
+      [Test]
+      public void Equals_DiffComplexObj5_ReturnsFalse() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+         var obj2 = new DummyClass(1, "str", null, new List<int> { 1, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, obj2);
+
+
+         // ASSERT
+         Assert.IsFalse(result);
+      }
+
+
+
+      [Test]
+      public void Equals_DiffComplexObj6_ReturnsFalse() {
+         // ARRANGE
+         var obj1 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 1, 2, 3 });
+         var obj2 = new DummyClass(1, "str", new[] { "str1", "str2" }, new List<int> { 2, 2, 3 });
+
+
+         // ACT
+         var result = Equals(obj1, obj2);
+
+
+         // ASSERT
+         Assert.IsFalse(result);
+      }
+
+   }
+}
